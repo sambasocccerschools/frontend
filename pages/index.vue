e3178f5ff728972f3a967e83ec23256d942fad2c
robyifan <lastresmoyallc@gmail.com>
2023-08-15 20:50:44 -0600

    blog section on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -138,6 +138,55 @@
             </div>
          </div>
       </section>
+      <!-- Check out our Blogs -->
+      <section class="py-5 my-4">
+         <div class="container">
+            <div class="text-center">
+               <h2 class="h1"><span class="font-custom text-primary">Check out our</span> <span class="text-success">BLOGS</span></h2>
+               <p class="text-muted col-sm-9 mx-auto">Our weekly blog brings you everything you need to know about children’s football training in London, as well as tips and in-depth footballing breakdowns that can help your child reach the next level.</p>
+            </div>
+            <div class="row my-5 align-items-start">
+               <div class="col">
+                  <img src="@/src/assets/img-blog-1.png" alt="" class="img-fluid">
+                  <h4 class="font-custom text-primary mt-4">How to Plan Football Sessions for Children?</h4>
+                  <p class="text-muted lead">Planning a football session for kids can seem daunting but it doesn’t have to be. The following article will provide a...</p>
+               </div>
+               <div class="col d-grid">
+                  <div class="row align-items-start">
+                     <img src="@/src/assets/img-blog-2.png" alt="" class="col-sm-5">
+                     <div class="col">
+                        <h4 class="text-primary h6 m-0">Football Skills to Practice in the Playground</h4>
+                        <p><small>Every kid loves pulling off fancy tricks and skills and there’s no better place to try these out than on the playgroun...</small></p>
+                     </div>
+                  </div>
+                  <div class="row align-items-start">
+                     <img src="@/src/assets/img-blog-3.png" alt="" class="col-sm-5">
+                     <div class="col">
+                        <h4 class="text-primary h6 m-0">5 Ways The SSS Skills Tutorial App Can Improve Your Child’s Football Skills</h4>
+                        <p><small>The Samba Soccer Skills Tutorial App is our home for weekly skills and tutorials. The SSS app...</small></p>
+                     </div>
+                  </div>
+                  <div class="row align-items-start">
+                     <img src="@/src/assets/img-blog-4.png" alt="" class="col-sm-5">
+                     <div class="col">
+                        <h4 class="text-primary h6 m-0">Six ways to manipulate the ball with a left foot or you know</h4>
+                        <p><small>Everyone loves scoring goals and there’s no position that is focussed more on scoring goals than a striker. ...</small></p>
+                     </div>
+                  </div>
+                  <div class="row align-items-start">
+                     <img src="@/src/assets/img-blog-5.png" alt="" class="col-sm-5">
+                     <div class="col">
+                        <h4 class="text-primary h6 m-0">5 Important Attributes the Best Strikers Need</h4>
+                        <p><small>Everyone loves scoring goals and there’s no position that is focussed more on scoring goals than a striker. ...</small></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="text-center">
+               <NuxtLink to="/blog" class="btn btn-primary btn-lg rounded-5 px-4 py-2"><span class="font-custom">Check Out Our Blogs</span></NuxtLink>
+            </div>
+         </div>
+      </section>
    </NuxtLayout>
 </template>
 
