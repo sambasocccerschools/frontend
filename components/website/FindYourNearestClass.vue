7171245f5d2e01e2892a1708b89bbf2f86df5649
robyifan <lastresmoyallc@gmail.com>
2023-08-15 19:19:15 -0600

    services section

--- components/website/FindYourNearestClass.vue
+++ components/website/FindYourNearestClass.vue
@@ -3,17 +3,17 @@
 <template>
   <div class="container find-your-nearest-class ">
     <div class="col-sm-9 mx-auto">
-      <div class="card rounded-4 shadow-lg">
-        <div class="card-body pt-0 px-4">
+      <div class="card rounded-5 shadow-lg border-0">
+        <div class="card-body pt-0 px-5">
           <form action="">
-            <p class="h2 text-center mx-5 custom-border pt-4 pb-4 text-primary">Find your nearest class</p>
+            <p class="h2 text-center mx-3 custom-border pt-4 pb-4 text-primary">Find your nearest class</p>
             <div class="row">
               <div class="col">
                 <input type="text" placeholder="Enter your postcode" class="form-control">
               </div>
               <div class="col">
                 <select name="" id="" placeholder="Or select a venue" class="form-select">
-                    <option value="">Some venues</option>
+                  <option value="">Some venues</option>
                 </select>
               </div>
             </div>
@@ -34,7 +34,7 @@
   }
   .custom-border {
     border-width: 10px;
-    border-top: .6rem solid $warning;
+    border-top: .7rem solid $warning;
   }
    // .card {
    //    box-shadow: 0 .2rem 4rem rgba(0,0,0,.2);
--- pages/index.vue
+++ pages/index.vue
@@ -11,22 +11,33 @@
       </header>
       <div>
          <WebsiteFindYourNearestClass/>
-         <div>
-            <img src="@/src/assets/player-pointing.png" alt="Samba Soccer Schools Player - Building confidence, mastering skills">
-            <div>
-               <h2>Building confidence, mastering skills - doing it in Style</h2>
-               <p>Samba Soccer Schools - more than just football coaching. Improving your child’s social skills and confidence, developing technique, flair, and footballing ability through our unique Brazilian style.</p>
+         <div class="container bg-player">
+            <div class="row">
+               <div class="col-sm-7 offset-sm-4 text-center">
+                  <h2 class="display-4 text-primary"><strong>Building confidence, mastering skills - doing it in Style</strong></h2>
+                  <p class="lead text-muted">Samba Soccer Schools - more than just football coaching. Improving your child’s social skills and confidence, developing technique, flair, and footballing ability through our unique Brazilian style.</p>
+               </div>
             </div>
          </div>
          <!-- Our Services  -->
-         <section>
-            <h2>Our Services</h2>
-            <p>Take your pick. We offer football training in London with weekly classes, one-to-one sessions, holiday camps, and birthday parties, all set to a jumping samba beat. </p>
-            <div class="services">
-               <NuxtLink to="services/weekly-classes" class="service-card"><span><span>Weekly classes </span> <span>More info</span>  </span></NuxtLink>
-               <NuxtLink to="services/holiday-camps" class="service-card"><span><span>Holiday Camps </span> <span>More info</span>  </span></NuxtLink>
-               <NuxtLink to="services/one-to-one-training" class="service-card"><span><span>One-to-one training </span> <span>More info</span>  </span></NuxtLink>
-               <NuxtLink to="services/birthday-parties" class="service-card"><span><span>Birthday parties </span> <span>More info</span>  </span></NuxtLink>
+         <section class="bg-primary py-5">
+            <div class="container text-center">
+               <h2 class="h1"><span class="text-light">Our</span> <span class="text-warning">Services</span></h2>
+               <p class="text-light mb-5">Take your pick. We offer football training in London with weekly classes, one-to-one sessions, holiday camps, and birthday parties, all set to a jumping samba beat. </p>
+               <div class="row row-cols-sm-2 row-cols-1 g-3">
+                  <div class="col">
+                     <NuxtLink to="services/weekly-classes" class="service-card rounded-4 bg-warning"><span><span>Weekly classes </span> <span>More info</span>  </span></NuxtLink>
+                  </div>
+                  <div class="col">
+                     <NuxtLink to="services/holiday-camps" class="service-card rounded-4 bg-success"><span><span>Holiday Camps </span> <span>More info</span>  </span></NuxtLink>
+                  </div>
+                  <div class="col">
+                     <NuxtLink to="services/one-to-one-training" class="service-card rounded-4 bg-primary-subtle"><span><span>One-to-one training </span> <span>More info</span>  </span></NuxtLink>
+                  </div>
+                  <div class="col">
+                     <NuxtLink to="services/birthday-parties" class="service-card rounded-4 bg-info"><span><span>Birthday parties </span> <span>More info</span>  </span></NuxtLink>
+                  </div>
+               </div>
             </div>
          </section>
          <!-- Reviews  -->
@@ -59,15 +70,21 @@ Booking a free trial only takes two minutes, and with venues across London, ther
       background-size: cover;
       height: 70vh;
    }
-   .services {
-      text-align: center;
+   .bg-player {
+      background-image: url('@/src/assets/player-pointing.png');
+      background-repeat: no-repeat;
+      background-size: auto 100%;
+      background-position: center left;
+      padding-top: 4rem;
+      padding-bottom: 7rem;
    }
    .service-card {
       background: #dadada;
-      width: 30rem;
-      height: 15rem;
-      display: inline-block;
-      margin: .5rem;
+      height: 16rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
    }
    section {
       &.book-a-free-trial {
