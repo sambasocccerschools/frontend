8217da51933c3ed8e0c3bd39e3769577465691e5
robyifan <lastresmoyallc@gmail.com>
2023-08-17 06:54:29 -0600

    find a class

--- components/website/FindYourNearestClass.vue
+++ components/website/FindYourNearestClass.vue
@@ -6,7 +6,7 @@
 
       <div class="card rounded-5 shadow-lg border-0">
         <div class="card-body pt-0 px-5">
-          <form action="">
+          <form action="/find-a-class">
             <p class="h2 text-center mx-3 custom-border pt-4 pb-4 text-primary">Find your nearest class</p>
             <div class="row">
               <div class="col">
@@ -19,7 +19,7 @@
               </div>
             </div>
             <div class="d-grid pt-4 pb-4">
-              <button class="btn btn-success rounded-5 btn-lg text-white"><strong>Search</strong></button>
+              <button class="btn btn-success rounded-5 btn-lg text-white" type="submit"><strong>Search</strong></button>
             </div>
           </form>
         </div>
