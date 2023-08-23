50e13a69f8aa944f5e0d89a42a49897ff2d3adaf
robyifan <lastresmoyallc@gmail.com>
2023-08-22 22:59:19 -0600

    venues

--- components/synco/synco-navigation.vue
+++ components/synco/synco-navigation.vue
@@ -1,70 +1,76 @@
 <template>
-  <div class="vh-100 border-end border-1 border-secondary d-flex flex-column py-5 px-4 bg-white overflow-y-scroll"
+  <div
+    class="vh-100 border-end border-1 border-secondary d-flex flex-column py-5 px-4 bg-white overflow-y-scroll justify-content-between"
     style="width:18rem">
-    <div class="text-center mb-4">
-      <img src="@/src/assets/sss-logo-synco-black.png" alt="Synco logo" class="w-50">
-    </div>
-    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample2"
-      aria-expanded="false" aria-controls="multiCollapseExample2">Toggle second element</button>
-    <div class="collapse multi-collapse" id="multiCollapseExample2">
-      <div class="card card-body">
-        Some placeholder content for the second collapse component of this multi-collapse example. This panel is hidden by
-        default but revealed when the user activates the relevant trigger.
+    <div>
+      <div class="text-center mb-4">
+        <img src="@/src/assets/sss-logo-synco-black.png" alt="Synco logo" class="w-50">
       </div>
+
+      <NuxtLink to="/synco/dashboard" exact-active-class="btn-nav-active" class="btn-nav">
+        <Icon name="akar-icons:grid" /> Dashboard
+      </NuxtLink>
+
+      <!-- Weekly Classes -->
+      <button class="btn btn-nav" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWeeklyClasses"
+        aria-expanded="false" aria-controls="collapseWeeklyClasses">
+        <Icon name="ph:book-open-bold" />Weekly Classes
+        <Icon name="pajamas:chevron-down" />
+      </button>
+      <ul class="ms-4 collapse " id="collapseWeeklyClasses">
+        <li>
+          <NuxtLink to="/synco/weekly-classes/find-a-class" class="btn-nav-link" exact-active-class="text-primary">
+            Find a class
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/synco/weekly-classes/members" class="btn-nav-link" exact-active-class="text-primary">
+            Members
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/synco/weekly-classes/sales" class="btn-nav-link" exact-active-class="text-primary">Sales
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/synco/weekly-classes/trials" class="btn-nav-link" exact-active-class="text-primary">Trials
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/synco/weekly-classes/cancellation" class="btn-nav-link" exact-active-class="text-primary">
+            Cancellation
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/synco/weekly-classes/waiting-list" class="btn-nav-link" exact-active-class="text-primary">
+            Waiting
+            List
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/synco/weekly-classes/leads-database" class="btn-nav-link" exact-active-class="text-primary">
+            Leads
+            Database
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/synco/weekly-classes/capacity" class="btn-nav-link" exact-active-class="text-primary">
+            Capacity
+          </NuxtLink>
+        </li>
+      </ul>
+
+      <NuxtLink to="/synco/one-to-one" exact-active-class="btn-nav-active" class="btn-nav">
+        <Icon name="ph:book-open-bold" /> One to One
+      </NuxtLink>
+
+      <NuxtLink to="/synco/venues" exact-active-class="btn-nav-active" class="btn-nav">
+        <Icon name="ph:map-pin" /> Venues
+      </NuxtLink>
     </div>
-    <NuxtLink to="/synco/dashboard" exact-active-class="btn-nav-active" class="btn-nav">
-      <Icon name="akar-icons:grid" /> Dashboard
-    </NuxtLink>
-    <!-- Weekly Classes -->
-    <button class="btn-nav border-0">
-      <Icon name="ph:book-open-bold" />Weekly Classes
-    </button>
-    <ul class="ms-4">
-      <li>
-        <NuxtLink to="/synco/weekly-classes/find-a-class" class="btn-nav-link" exact-active-class="text-primary">
-          Find a class
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/synco/weekly-classes/members" class="btn-nav-link" exact-active-class="text-primary">
-          Members
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/synco/weekly-classes/sales" class="btn-nav-link" exact-active-class="text-primary">Sales
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/synco/weekly-classes/trials" class="btn-nav-link" exact-active-class="text-primary">Trials
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/synco/weekly-classes/cancellation" class="btn-nav-link" exact-active-class="text-primary">
-          Cancellation
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/synco/weekly-classes/waiting-list" class="btn-nav-link" exact-active-class="text-primary">
-          Waiting
-          List
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/synco/weekly-classes/leads-database" class="btn-nav-link" exact-active-class="text-primary">
-          Leads
-          Database
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/synco/weekly-classes/capacity" class="btn-nav-link" exact-active-class="text-primary">
-          Capacity
-        </NuxtLink>
-      </li>
-    </ul>
 
 
-    <NuxtLink to="/synco/venues">Venues</NuxtLink>
-    <NuxtLink to="/synco">Logout</NuxtLink>
+    <NuxtLink to="/synco" class="btn btn-outline-dark"><span>Logout</span></NuxtLink>
   </div>
 </template>
 <style lang="scss" scoped>
@@ -73,7 +79,7 @@
 .btn-nav {
   @extend .btn;
   @extend .btn-lg;
-  @extend .py-3;
+  @extend .py-2;
   text-align: start;
   align-items: center;
   display: flex;
@@ -91,6 +97,7 @@
     @extend .text-light;
     @extend .rounded-4;
     box-shadow: 4px 6px 12px 0px rgba(35, 127, 234, 0.25);
+    @extend .mb-1;
   }
 }
 </style>
--- components/website/WebsiteHeader.vue
+++ components/website/WebsiteHeader.vue
@@ -26,6 +26,14 @@
           .
           <NuxtLink to="/services/weekly-classes" class="btn btn-link text-light" active-class="current">Services
           </NuxtLink>
+          <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </button>
+          <ul class="dropdown-menu dropdown-menu-dark">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
           .
           <NuxtLink to="/about-us" class="btn btn-link text-light" active-class="current">About Us</NuxtLink>
         </div>
