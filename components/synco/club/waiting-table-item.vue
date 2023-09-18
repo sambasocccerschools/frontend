8ab4c7cfc0ade79b540e102b8db5576cb0c62887
robyifan <lastresmoyallc@gmail.com>
2023-09-17 23:56:11 -0600

    club waiting list

---
+++ components/synco/club/waiting-table-item.vue
@@ -0,0 +1,17 @@
+<template>
+  <tr class="align-middle">
+    <!-- <th scope="row">
+      <input class="form-check-input" type="checkbox" value="" id="tomjones">
+    </th> -->
+    <th scope="row">Steve Jones</th>
+    <!-- <td><label class="form-check-label text-muted" for="tomjones">
+        Tom Jones
+      </label></td> -->
+    <td>15</td>
+    <td>Acton</td>
+    <td>Sat 1st Nov 22</td>
+    <td>Jaffar Bukari</td>
+    <td>1</td>
+    <td>High</td>
+  </tr>
+</template>
