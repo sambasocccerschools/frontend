1f6296b3a6a6d2b5c47e537f0275a8c132f05a3d
robyifan <lastresmoyallc@gmail.com>
2023-09-24 23:43:10 -0600

    recruitment wip

---
+++ components/synco/recruitment/leads-table-item.vue
@@ -0,0 +1,28 @@
+<template>
+  <tr class="align-middle">
+    <!-- <th scope="row">
+      <input class="form-check-input" type="checkbox" value="" id="tomjones">
+    </th> -->
+    <th scope="row">Tom Jones</th>
+    <!-- <td><label class="form-check-label text-muted" for="tomjones">
+        Tom Jones
+      </label></td> -->
+    <td>25</td>
+    <td>W14 9EB</td>
+    <td>12344567</td>
+    <td>tom.john@gmail.com</td>
+    <td>2 years</td>
+    <td>
+      <Icon class="text-success" name="lucide:check-circle-2" />
+    </td>
+    <td>
+      <Icon class="text-danger" name="pepicons-pop:times-circle" />
+    </td>
+    <td>
+      <span class="badge bg-warning-subtle px-1 text-warning">
+        Pending
+      </span>
+    </td>
+    <!-- pending , Rejected, recruited-->
+  </tr>
+</template>
