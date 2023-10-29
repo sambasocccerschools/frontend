ae7bc6f8ef48ce69d27e348957c2f6dd008e22c2
Rob Moya <lastresmoyallc@gmail.com>
2023-10-29 01:03:21 -0600

    dates holiday camps config

--- pages/synco/config/holiday-camps/dates.vue
+++ pages/synco/config/holiday-camps/dates.vue
@@ -1,53 +1,113 @@
+<script>
+export default {
+  data: () => ({
+    // audiences: 8
+    dates: [
+      { name: 'Autumn 23 Saturdays', sessions: 4 },
+    ]
+  }),
+  methods: {
+    duplicateTerm() { console.log('duplicate') },
+    sendToTrash() { console.log('sendToTrash') },
+    saveTerm() { console.log('save Term') }
+  }
+}
+</script>
+
+
+
 <template>
-   <NuxtLayout name="syncolayout">
-      <div class="container-fluid">
-         <div class="row">
-            <div class="col">
-               <h4 class="mb-4">Term Dates & Mapping Session Plans</h4>
-               <h5 class="mb-4">Add a term</h5>
-               <div class="card">
-                  <div class="card-body p-4">
-                     <div class="row row-cols-2 gy-4">
-                        <div class="col">
-                           <label for="term-name">Term name</label>
-                           <input type="text" class="form-control mt-2" id="term-name" placeholder="Autumn 23 Saturdays"
-                              name="term-name">
+  <NuxtLayout name="syncolayout">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+          <h4 class="mb-4">Dates & Mapping Session Plans</h4>
+          <h5 class="mb-4">Add Dates</h5>
+
+
+          <!-- Date Card -->
+          <div class="card mb-3" v-for="date in dates">
+            <div class="card-body p-4">
+              <div class="row row-cols-2 gy-4">
+                <div class="col">
+                  <label for="camp-name">Holiday Camp Name</label>
+                  <input type="text" class="form-control mt-2" id="camp-name" placeholder="Autumn 23 Saturdays"
+                    name="camp-name">
+                </div>
+                <div class="col">
+                  <label for="camp">Camp</label>
+                  <input type="text" class="form-control mt-2 border-danger" id="camp" placeholder="Autumn" name="camp">
+                </div>
+                <div class="col">
+                  <label for="start-date">Start date</label>
+                  <input type="date" class="form-control mt-2" id="start-date" name="start-date">
+                  <!-- <input type="date" id="start" name="trip-start" value="2018-07-22" min="2018-01-01" max="2018-12-31" /> -->
+                </div>
+                <div class="col">
+                  <label for="end-date">End date</label>
+                  <input type="date" class="form-control mt-2" id="end-date" name="end-date">
+                </div>
+                <div class="col">
+                  <label for="duration">Duration</label>
+                  <input type="date" class="form-control mt-2 border-danger" id="duration" name="duration">
+                </div>
+              </div>
+              <div class="d-flex align-items-center justify-content-between mt-4">
+                <NuxtLink to="" class="btn btn-outline-primary border-danger">
+                  Map
+                  Session</NuxtLink>
+                <div>
+                  <button @click="duplicateTerm()" class="btn btn-outline-primary">Duplicate</button>
+                  <button @click="sendToTrash()" class="btn btn-link text-dark">
+                    <Icon name="ph:trash-thin" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <button class="btn btn-primary btn-lg text-light" @click="dates.push({})">Add New Dates</button>
+        </div>
+        <div class="col">
+          <h4 class="mb-4">Map Sessions Plans</h4>
+
+          <div class="card mb-4" v-for="date in dates">
+            <div class="card-header">
+              <h4 class="card-title mt-3 h5">{{ date.name }}</h4>
+
+            </div>
+            <div class="card-body">
+              <div class="card-body px-2">
+                <div class="row row-cols-2 gy-2">
+                  <div v-for="session in date.sessions" class="col">
+                    <div class="card border">
+                      <div class="card-body py-2">
+                        <div class="d-flex justify-content-between align-items-center">
+                          <strong>Date 1</strong>
+                          <button class="btn btn-link p-0 btn-sm text-dark">
+                            <Icon name="ph:pencil-simple-line" />
+                          </button>
                         </div>
-                        <div class="col">
-                           <label for="term">Term</label>
-                           <input type="text" class="form-control mt-2" id="term" placeholder="Autumn" name="term">
+                        <div class="d-flex justify-content-between align-items-center">
+                          <small class="text-muted">Saturday, 9/9/2023, 11:00</small>
+                          <button class="btn btn-link p-0 btn-sm text-warning">
+                            <Icon name="ic:round-access-time-filled" />
+                          </button>
                         </div>
-                        <div class="col">
-                           <label for="start-date">Start date</label>
-                           <input type="date" class="form-control mt-2" id="start-date" name="start-date">
-                           <!-- <input type="date" id="start" name="trip-start" value="2018-07-22" min="2018-01-01" max="2018-12-31" /> -->
-                        </div>
-                        <div class="col">
-                           <label for="end-date">End date</label>
-                           <input type="date" class="form-control mt-2" id="end-date" name="end-date">
-                        </div>
-                        <div class="col">
-                           <label for="half-term-exclusion-date">Half-Term Exclusion Date(s)</label>
-                           <input type="date" class="form-control mt-2" id="half-term-exclusion-date"
-                              name="half-term-exclusion-date">
-                        </div>
-                        <div class="col">
-                           <label for="total-number-of-sessions">Total Number of Sessions</label>
-                           <input type="number" class="form-control mt-2" id="total-number-of-sessions"
-                              name="total-number-of-sessions">
-                        </div>
-                     </div>
-                     <div class="d-flex align-items-center justify-content-between">
-                        <NuxtLink to="/synco/config/ven">Map Session</NuxtLink>
-                     </div>
+                      </div>
+                    </div>
                   </div>
-               </div>
+                </div>
+              </div>
             </div>
-            <div class="col">
-               <h4>Map Sessions Plans</h4>
-            </div>
-         </div>
+          </div>
+
+          <div class="d-flex justify-content-end">
+            <button @click="saveTerm()" class="btn btn-primary btn-lg text-light px-5">Save</button>
+          </div>
+        </div>
       </div>
-   </NuxtLayout>
+    </div>
+  </NuxtLayout>
 </template>
  
