8dd63991a403984cc9b6ea0a4081d37a4325c4dd
Kevin <ild.freelancer@gmail.com>
2024-03-11 19:23:00 +0700

    fix: delete tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['nuxt-icon', '@pinia/nuxt', 'dayjs-nuxt', '@nuxtjs/tailwindcss'],
+  modules: ['nuxt-icon', '@pinia/nuxt', 'dayjs-nuxt'],
   dayjs: {
     plugins: ['relativeTime', 'customParseFormat'],
   },
