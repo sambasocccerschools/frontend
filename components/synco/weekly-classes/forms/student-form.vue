997c8461bc02d0a121f5d36faa04329336067c84
JuanDiegoMoralesDelCorral <dmdelcor@gmail.com>
2024-04-09 19:05:08 -0700

    Add new Lead - added Components and Models

---
+++ components/synco/weekly-classes/forms/student-form.vue
@@ -0,0 +1,150 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import type { IStudent } from '~/types/index'
+const props = defineProps<{
+    student: IStudent
+}>()
+
+let student = ref<IStudent>(props.student).value
+
+const activities  = ref([
+    {label:"Select from drop down", value:""},
+    {label:"Weekly classes", value:"Weekly classes"},
+    {label:"One to one", value:"One to one"},
+    {label:"Holiday camps", value:"Holiday camps"},
+    {label:"Clubs", value:"Clubs"},
+    {label:"Merchandise", value:"Merchandise"},
+])
+
+const addStudent = ()=>{
+    console.log("add student")
+}
+
+</script>
+
+<template>
+    <h4 class="mt-4"><strong>Students</strong></h4>
+    <div class="card rounded-4 mt-4 px-3">
+        <h5 class="py-4"><strong>Student information</strong></h5>
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group mb-3 w-100">
+                    <label for="studentFirstName" class="form-labelform-label-light"
+                    >First name</label
+                    >
+                    <input
+                    id="studentFirstName"
+                    type="text"
+                    class="form-control form-control-lg"
+                    placeholder="Enter first name"
+                    v-model="student.firstName"
+                    />
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="form-group mb-3 w-100">
+                    <label for="studentLastName" class="form-labelform-label-light"
+                    >Last name</label
+                    >
+                    <input
+                    id="studentLastName"
+                    type="text"
+                    class="form-control form-control-lg"
+                    placeholder="Enter last name"
+                    v-model="student.lastName"
+                    />
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group mb-3 w-100">
+                    <label for="studentDoB" class="form-labelform-label-light"
+                    >Date of Birth</label
+                    >
+                    <input
+                    id="studentDoB"
+                    type="date"
+                    class="form-control form-control-lg"
+                    placeholder="Enter date of birth"
+                    v-model="student.dateOfBirth"
+                    />
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="form-group mb-3 w-100">
+                    <label for="studentAge" class="form-labelform-label-light"
+                    >Age</label
+                    >
+                    <input
+                    id="studentAge"
+                    type="text"
+                    class="form-control form-control-lg"
+                    placeholder="Automatic entry"
+                    v-model="student.age"
+                    readonly
+                    />
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group mb-3 w-100">
+                    <label for="studentGender" class="form-labelform-label-light"
+                    >Gender</label
+                    >
+                    <input
+                    id="studentGender"
+                    type="text"
+                    class="form-control form-control-lg"
+                    placeholder="Enter gender"
+                    v-model="student.gender"
+                    />
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="form-group mb-3 w-100">
+                    <label for="studentMedicalInformation" class="form-labelform-label-light"
+                    >Medical information</label
+                    >
+                    <input
+                    id="studentMedicalInformation"
+                    type="text"
+                    class="form-control form-control-lg"
+                    placeholder="Enter medical information"
+                    v-model="student.medicalInformation"
+                    />
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group mb-3 w-100">
+                    <label for="studentAoI" class="form-labelform-label-light"
+                    >Activity of interest</label
+                    >
+                    <select
+                    id="studentAoI"
+                    class="form-control form-control-lg"
+                    v-model="student.activityOfInterest"
+                    >
+                    <option v-for="(activity,index) in activities" :value="activity.value" :key="index">{{activity.label}}</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-6">
+            </div>
+        </div>
+    </div>
+    <div class="card rounded-4 mt-4 px-3">
+        <div class="d-flex justify-content-between my-4">
+            <button
+                class="btn btn-primary text-light btn-lg"
+                @click="addStudent"
+                >
+                + Add new student
+            </button>
+        </div>
+    </div>
+</template>
---
+++ components/synco/weekly-classes/parent-list-item.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+
+import type { IParentListItem } from '~/types/index'
+const props = defineProps<{
+    parent: IParentListItem
+}>()
+
+const selectParent = (selectedParent:IParentListItem)=>{
+    console.log(selectedParent)
+}
+</script>
+
+<template>
+    <div class="card-body d-flex align-items-center justify-content-start py-4">
+        <div class="d-flex col-auto">
+            <div class="d-flex flex-column px-3">
+                <label>Name</label>
+                {{parent["First Name"]}} {{parent["Last Name"]}}
+            </div>
+            <div class="vr"></div>
+            <div class="d-flex flex-column px-3">
+                <label>Status</label>
+                {{parent["Status"]}}
+            </div>
+            <div class="vr"></div>
+            <div class="d-flex flex-column px-3">
+                <label>Email</label>
+                {{parent["Email"]}}
+            </div>
+            <div class="vr"></div>
+            <div class="d-flex flex-column px-3">
+                <label>Gocardless ID</label>
+                {{ parent["Gocardless ID"] }}
+            </div>
+            <div class="vr"></div>
+            <div class="d-flex flex-column px-3">
+                <label>Created</label>
+                {{parent["Created"]}}
+            </div>
+            <div class="vr"></div>
+            <div class="d-flex flex-column px-3">
+                <label>Address</label>
+                {{parent["Address"]}}
+            </div>
+            <div class="vr"></div>
+            <div class="d-flex flex-column px-3">
+                <label>Phone number</label>
+                {{parent["Phone number"]}}
+            </div>
+        </div>
+        <button
+            @click="selectParent(parent)"
+            class="btn btn-primary text-light ms-auto"
+            >Select parent</button
+        >
+        </div>
+        <template v-if="!!parent['Students']">
+            <div class="d-flex flex-row col-auto">
+                <div v-for="(student,sindex) in parent['Students']" class="card rounded-4 mb-4 border mx-3 w-100" style="background-color: #fcf9f6;" :key="sindex">
+                    <h6 class="px-4 pt-4 pb-2"><strong>Student {{ sindex+1 }}</strong></h6>
+                    <div class="d-flex flex-row pb-4">
+                        <div class="d-flex flex-column px-3">
+                            <label>Name</label>
+                            {{student["First Name"]}} {{student["Last Name"]}}
+                        </div>
+                        <div class="d-flex flex-column px-3">
+                            <label>Age</label>
+                            {{student["Age"]}}
+                        </div>
+                        <div class="d-flex flex-column px-3">
+                            <label>Gender</label>
+                            {{student["Gender"]}}
+                        </div>
+                        <div class="d-flex flex-column px-3">
+                            <label>Date of Birth</label>
+                            {{student["Date of Birth"]}}
+                        </div>
+                        <div class="d-flex flex-column px-3">
+                            <label>Activity of interest</label>
+                            {{student["Activity of interest"]}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </template>
+</template>
